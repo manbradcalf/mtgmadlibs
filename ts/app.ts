0be0f0b4efa15a1d505376cbd585a1aef4c0283c
manbradcalf <ben.medcalf@gmail.com>
2021-12-19 15:14:21 -0500

    WIP

--- ts/app.ts
+++ ts/app.ts
@@ -8,16 +8,6 @@ function r(list: Card[]) {
   return list[Math.floor(Math.random() * list.length)].name;
 }
 
-// const cardsAndTypes = Object.values(cards.data as card[]).map((c) => ({
-//   name: c[0].name,
-//   type: c[0].type,
-// }));
-
-// Deno.writeTextFileSync(
-//   "./modernCardsAndTypes.json",
-//   JSON.stringify(cardsAndTypes)
-// );
-
 const artifacts = cards.filter((card) =>
   card.type.includes("Artifact")
 );
@@ -37,8 +27,11 @@ const instantsAndSorceries = cards.filter(
 );
 
 function generateMadlib(rawText: string): string {
+  //TODO: This method uses the same type for each instance.
+  // Ex. My $creature and his friend $Creature returns
+  // My Llanowar Elves and his friend Llanowar Elves
   rawText = rawText.replace(/\$creature/ig, r(creatures));
-  rawText = rawText.replace(/\$artifcate/ig, r(artifacts));
+  rawText = rawText.replace(/\$artifact/ig, r(artifacts));
   rawText = rawText.replace(/\$land/ig, r(lands));
   rawText = rawText.replace(/\$enchantment/ig, r(enchantments));
   rawText = rawText.replace(/\$instantOrSorcery/ig, r(instantsAndSorceries));
