183c475665b3ef78d1a68a3ed2163382efff8326
ben-medcalf <ben_medcalf@carmax.com>
2021-12-27 12:07:30 -0500

    replace each instance of regex match with random card

--- ts/app.ts
+++ ts/app.ts
@@ -1,40 +1,41 @@
+import { cards } from "../data/modernCardsAndTypes.js";
+
 type Card = {
   name: string;
   type: string;
 };
 
-import {cards} from "../data/modernCardsAndTypes.js"
 function r(list: Card[]) {
   return list[Math.floor(Math.random() * list.length)].name;
 }
 
-const artifacts = cards.filter((card) =>
-  card.type.includes("Artifact")
-);
+const artifacts = cards.filter((card) => card.type.includes("Artifact"));
 
-const creatures = cards.filter((card) =>
-  card.type.includes("Creature")
-);
+const creatures = cards.filter((card) => card.type.includes("Creature"));
 
 const lands = cards.filter((card) => card.type.includes("Land"));
 
-const enchantments = cards.filter((card) =>
-  card.type.includes("Enchantment")
-);
+const enchantments = cards.filter((card) => card.type.includes("Enchantment"));
 
 const instantsAndSorceries = cards.filter(
   (card) => card.type.includes("Instant") || card.type.includes("Sorcery")
 );
 
 function generateMadlib(rawText: string): string {
-  //TODO: This method uses the same type for each instance.
-  // Ex. My $creature and his friend $Creature returns
-  // My Llanowar Elves and his friend Llanowar Elves
-  rawText = rawText.replace(/\$creature/ig, r(creatures));
-  rawText = rawText.replace(/\$artifact/ig, r(artifacts));
-  rawText = rawText.replace(/\$land/ig, r(lands));
-  rawText = rawText.replace(/\$enchantment/ig, r(enchantments));
-  rawText = rawText.replace(/\$instantOrSorcery/ig, r(instantsAndSorceries));
+  // Nest the call to r() in an anonymous function to execute on each match
+  rawText = rawText.replace(/\$creature/gi, () => r(creatures));
+  rawText = rawText.replace(/\$artifact/gi, () => r(artifacts));
+  rawText = rawText.replace(/\$land/gi, () => r(lands));
+  rawText = rawText.replace(/\$enchantment/gi, () => r(enchantments));
+  rawText = rawText.replace(/\$instantOrSorcery/gi, () => r(instantsAndSorceries));
   return rawText;
 }
-export { r, artifacts, creatures, lands, enchantments, instantsAndSorceries, generateMadlib };
+export {
+  r,
+  artifacts,
+  creatures,
+  lands,
+  enchantments,
+  instantsAndSorceries,
+  generateMadlib,
+};
